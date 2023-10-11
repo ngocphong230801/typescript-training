70c5d2e7ebf3db703513ec7dd9e7484fe56542c6
ngocphong230801 <nnphong962015@gmail.com>
2023-10-11 14:06:59 +0700

    Set loading when reload web

--- practice/src/scripts/views/task.view.ts
+++ practice/src/scripts/views/task.view.ts
@@ -1,13 +1,20 @@
+import { querySelector } from "../helpers/dom-element";
+
 class TaskView {
-    
-    private loadingElement: HTMLElement;
+    private loadingElement: HTMLElement | null;
 
     constructor() {
         window.addEventListener("load", async () => {
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-            this.loadingElement.style.display = "none";
-        });
+            this.loadingElement = querySelector(".app__loading") as HTMLElement ;
 
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+
+            if (this.loadingElement) {
+                this.loadingElement.style.display = "none";
+            } else {
+                console.error("Loading element not found");
+            }
+        });
     }
 }
 
